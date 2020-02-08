622562a6a0b316bd082ae19d0d099c5307975276
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2020-02-08 20:04:28 +0700

    delete vendorsetup and add common lunch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_a33g.mk \
 	$(LOCAL_DIR)/pa_a33g.mk \
 	$(LOCAL_DIR)/lineage_a33g.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a33g-user \
+    lineage_a33g-userdebug \
+    lineage_a33g-eng
